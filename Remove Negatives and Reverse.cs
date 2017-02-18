3fde156b9024acd1bb980c0e8b87a2707dd67d5b
Dimitar Vatkov <vatkov.d@abv.bg>
2017-02-18 21:39:38 +0200

    Add files via upload

---
+++ Remove Negatives and Reverse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var result = new List<int>();
+            for (int i = n.Count; i > 0; i--)
+            {
+                if (n[i-1] >= 0)
+                {
+                    result.Add(n[i-1]);
+                }
+              
+            }
+            if (result.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+           
+        }
+    }
+}
